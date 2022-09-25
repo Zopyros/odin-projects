c4bca3c99e2d692c618477db3f9f2f8cd36df50a
Christian <cmanalo.io@outlook.com>
2022-09-25 15:19:54 -0700

    Create black hover and reset functionality

--- sketch/main.js
+++ sketch/main.js
@@ -1,4 +1,21 @@
 
+document.addEventListener("DOMContentLoaded",function(){
+
+    createContainer(16)
+let btn = document.querySelector('#popup');
+btn.addEventListener("click", function(){
+
+    let size = getSize()
+    createContainer(size)
+
+})
+
+
+})
+
+
+
+
 function createContainer(size){
     
 
@@ -10,9 +27,35 @@ let numDivs = size * size
 
 for (let i=0;i<numDivs;i++){
     let div = document.createElement("div");
-    div.style.backgroundColor ="blue";
+    div.addEventListener("mouseover",function(){
+        div.style.backgroundColor = "black"   
+    })
     contain.insertAdjacentElement("beforeend",div)
 
 }
 }
-createContainer(16);
+
+function getSize(){
+    let input = prompt("What Size will you choose?");
+    let message = document.querySelector("#message");
+    console.log(message)
+    if(input ==""){
+        message.innerHTML = "please provide a number";
+  
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "Please reenter a valid number from 1 - 99";
+        
+    }
+    else{
+        message.innerHTML = `You chose ${input} X ${input}`;
+        return Number(input)
+    }
+
+}
+
+function reset(){
+    let divs = document.querySelectorAll("div")
+    divs.forEach((div) => div.style.backgroundColor ="white")
+}
+
